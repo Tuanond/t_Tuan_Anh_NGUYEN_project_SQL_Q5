76001b2e22b44163c243367a7571f2034241a77c
Tuan Anh Nguyen <ondra.nguyen@gmail.com>
2024-06-22 20:26:16 +0200

    avg_payroll+avg_prices+GDP+gini GROUP by YEAR

--- t_Tuan_Anh_NGUYEN_project_SQL_Q5.sql
+++ t_Tuan_Anh_NGUYEN_project_SQL_Q5.sql
@@ -6,10 +6,33 @@ SELECT *
 FROM economies AS e
 WHERE country = 'Czech republic';
 
-SELECT *
+SELECT
+*
+FROM czechia_payroll AS cpay
+LEFT JOIN czechia_price AS cp 
+	ON cpay.payroll_year = YEAR(cp.date_from)
+ORDER BY cpay.payroll_year;
+
+SELECT
+	payroll_year,
+	avg(value)
+FROM czechia_payroll AS cp
+WHERE value_type_code = 5958
+GROUP BY payroll_year;
+
+SELECT
+	e.country,
+	e.YEAR,
+	avg(cpay.value) AS average_payroll,
+	avg(cp.value) AS average_prices,
+	e.GDP,
+	e.gini
 FROM czechia_payroll AS cpay
 LEFT JOIN czechia_price AS cp 
 	ON cpay.payroll_year = YEAR(cp.date_from)
 LEFT JOIN economies AS e
 	ON cpay.payroll_year = e.`year`
-WHERE e.country = 'Czech republic' AND cpay.value_type_code = "5958";
+WHERE e.country = 'Czech republic' AND cpay.value_type_code = "5958"
+GROUP BY e.year;
+
+
